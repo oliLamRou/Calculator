073def0758deaf51c93d790cb68e8f2d8f5c806d
needMDMA <parties01smacks@icloud.com>
2023-11-23 21:24:38 +0900

    very basic layout

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+let calculatorButtons = [
+        "c", "+/-", "%", "÷",
+        "9", "8", "7", "x",
+        "6", "5", "4", "-",
+        "3", "2", "1", "+",
+        "0", ".", "="
+]
+
+
+
+
+let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("1000").font(.largeTitle)
+            LazyVGrid(columns: columns) {
+                 ForEach(Array(calculatorButtons), id: \.self) { value in
+                     calculatorButton(value, .red)
+                 }
+            }.padding()
         }
-        .padding()
+    }
+    
+    /*:
+     -note:
+        - get the ZERO button to be large
+        - Scale text without changing layout
+        - get button color
+     */
+    
+    @ViewBuilder
+    func calculatorButton(_ value: String, _ color: Color) -> some View {
+        let color = Color.gray
+        ZStack {
+            Circle().foregroundStyle(.red)
+            Text(value)
+        }
     }
 }
 
