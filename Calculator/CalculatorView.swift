2b09c0efc27f8e4155a8e9e43a5eeed6a5a92e5b
needMDMA <parties01smacks@icloud.com>
2023-11-27 17:26:26 +0900

    foreach with enum working

--- Calculator/CalculatorView.swift
+++ Calculator/CalculatorView.swift
@@ -11,7 +11,7 @@ struct CalculatorView: View {
     @ObservedObject var calculatorLogic: CalculatorLogic
     
     private let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
-        
+    
     var zeroButton: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 40)
@@ -31,62 +31,65 @@ struct CalculatorView: View {
                 .ignoresSafeArea()
             VStack {
                 Spacer()
-                HStack {
-                    Spacer()
-                    Text(calculatorLogic.resultFormatted)
-                        .font(.system(size: 80))
-                        .bold()
-                        .foregroundStyle(.white)
-                        .padding(.vertical, -10)
-                }
-                LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem()]) {
-                    ForEach(buttons, id: \.self) { index in
-                        calculatorButton(index)
+                Text(calculatorLogic.resultFormatted)
+                    .font(.system(size: 80))
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding(.vertical, -10)
+                
+                VStack {
+                    ForEach(buttonLayout, id: \.self) { row in
+                        HStack {
+                            ForEach(row, id: \.self) { op in
+                                if op.rawValue == buttons.zero.rawValue {
+                                    
+                                }else {
+                                    calculatorButton(0)
+                                }
+
+                            }
+                        }
                     }
                 }
-                HStack {
-                    zeroButton
-                    calculatorButton(17)
-                    calculatorButton(18)
-                }
-            }
-            .padding()
-        }
-    }
-        
-    @ViewBuilder
-    func calculatorButton(_ index: Int) -> some View {
-        let value = calculatorButtons[index]
-        
-        var color: Color {
-            if index < 3 {
-                return .buttonGray
-            }
-            if [3,7,11,15,18].contains(where: {$0 == index}) {
-                return .buttonOrange
-            }
-            return Color(.buttonDarkGray)
-        }
-        
-        Button {
-            calculatorLogic.numberButton(index)
-        } label: {
-            ZStack {
-                if index == 16 {
-                    RoundedRectangle(cornerRadius: 50.0)
-                        .foregroundStyle(color)
-                        .frame(width: 100)
-                } else {
-                    Circle().foregroundStyle(color)
-                }
-                Text(value)
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
-                    .padding()
+                
+                .padding()
             }
         }
     }
-}
+        
+        @ViewBuilder
+        func calculatorButton(_ index: Int) -> some View {
+            let value = calculatorButtons[index]
+            
+            var color: Color {
+                if index < 3 {
+                    return .buttonGray
+                }
+                if [3,7,11,15,18].contains(where: {$0 == index}) {
+                    return .buttonOrange
+                }
+                return Color(.buttonDarkGray)
+            }
+            
+            Button {
+                calculatorLogic.numberButton(index)
+            } label: {
+                ZStack {
+                    if index == 16 {
+                        RoundedRectangle(cornerRadius: 50.0)
+                            .foregroundStyle(color)
+                            .frame(width: 100)
+                    } else {
+                        Circle().foregroundStyle(color)
+                    }
+                    Text(value)
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .padding()
+                }
+            }
+        }
+    }
 
 #Preview {
     CalculatorView(calculatorLogic: CalculatorLogic())
