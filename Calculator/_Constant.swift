29a035a052721ef7565d34beacc309b9c1ece3cf
needMDMA <parties01smacks@icloud.com>
2023-11-27 16:06:47 +0900

    creating button enum

--- Calculator/_Constant.swift
+++ Calculator/_Constant.swift
@@ -5,7 +5,7 @@
 //  Created by Olivier Lambert Rouillard on 2023-11-25.
 //
 
-import Foundation
+import SwiftUI
 
 let calculatorButtons = [
         "c", "+/-", "%", "÷",
@@ -30,24 +30,41 @@ struct button: Identifiable {
     let id: Int
 }
 
-let buttons = [
-    button(label: "c")
-    button(label: "/")
-    button(label: "%")
-    button(label: "÷")
-    button(label: "9")
-    button(label: "8")
-    button(label: "7")
-    button(label: "x")
-    button(label: "6")
-    button(label: "5")
-    button(label: "4")
-    button(label: "-")
-    button(label: "3")
-    button(label: "2")
-    button(label: "1")
-    button(label: "+")
-    button(label: "0")
-    button(label: ".")
-    button(label: "=")
+enum buttons: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eigh = "8"
+    case nine = "9"
+    case zero = "0"
+    case dot = "."
+    case clear = "c"
+    case invert = "+/-"
+    case equal = "="
+    case percent = "%"
+    case divide = "÷"
+    case multiply = "x"
+    case add = "+"
+    case minus = "-"
+    
+    var buttonColor: Color {
+        switch self {
+        case .add, .clear:
+            return .red
+        default:
+            return .green
+        }
+    }
+}
+
+let buttonLayout: [[buttons]] = [
+    [.clear, .invert, .percent, .divide],
+    [.seven, .eigh, .nine, .multiply],
+    [.four, .five, .six, .minus],
+    [.one, .two, .three, .add],
+    [.zero, .dot, .equal]
 ]
