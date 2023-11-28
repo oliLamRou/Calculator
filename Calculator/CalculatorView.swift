b00e3d8d9e7bbfc24f58f5daac821ad3a4ef81a6
needMDMA <parties01smacks@icloud.com>
2023-11-28 19:37:30 +0900

    enum done and that should have been done in UI branch

--- Calculator/CalculatorView.swift
+++ Calculator/CalculatorView.swift
@@ -8,9 +8,69 @@
 import SwiftUI
 
 struct CalculatorView: View {
+    enum buttons: String {
+        case one = "1"
+        case two = "2"
+        case three = "3"
+        case four = "4"
+        case five = "5"
+        case six = "6"
+        case seven = "7"
+        case eigh = "8"
+        case nine = "9"
+        case zero = "0"
+        case dot = "."
+        case clear = "c"
+        case invert = "+/-"
+        case equal = "="
+        case percent = "%"
+        case divide = "÷"
+        case multiply = "x"
+        case add = "+"
+        case minus = "-"
+        
+        var buttonColor: Color {
+            switch self {
+            case .clear, .invert, .percent:
+                return .buttonGray
+            case .divide, .multiply, .minus, .add, .equal:
+                return .buttonOrange
+            default:
+                return .buttonDarkGray
+            }
+        }
+        
+        var buttonWidth: CGFloat {
+            switch self {
+            case .zero:
+                return (buttonHeight * 2) + (buttonPadding * 2)
+            default:
+                return buttonHeight
+            }
+        }
+
+        var buttonHeight: CGFloat {
+            90
+        }
+        
+        var buttonPadding: CGFloat {
+            4
+        }
+    }
+
+    let buttonLayout: [[buttons]] = [
+        [.clear, .invert, .percent, .divide],
+        [.seven, .eigh, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .add],
+        [.zero, .dot, .equal]
+    ]
+    
     @ObservedObject var calculatorLogic: CalculatorLogic
     
     private let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    let buttonPadding: CGFloat = 0
+    
     
     var body: some View {
         ZStack {
@@ -19,54 +79,42 @@ struct CalculatorView: View {
                 .ignoresSafeArea()
             VStack {
                 Spacer()
-                Text(calculatorLogic.resultFormatted)
-                    .font(.system(size: 80))
-                    .bold()
-                    .foregroundStyle(.white)
-                    .padding(.vertical, -10)
-                
-                VStack {
+                HStack {
+                    Spacer()
+                    Text(calculatorLogic.resultFormatted)
+                        .font(.system(size: 80))
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding(.vertical, -10)
+                        .padding(.horizontal)
+                    
+                }
+                VStack(spacing: buttonPadding) {
                     ForEach(buttonLayout, id: \.self) { row in
-                        HStack {
+                        HStack(spacing: buttonPadding) {
                             ForEach(row, id: \.self) { op in
-                                if op.rawValue == buttons.zero.rawValue {
-                                    Button {
-                                        
-                                    } label: {
-                                        ZStack {
-                                            RoundedRectangle(cornerRadius: 50)
-                                                .frame(width: 180, height: 82)
-                                                .foregroundColor(.buttonDarkGray)
-                                            Text(op.rawValue)
-                                                .font(.largeTitle)
-                                                .foregroundStyle(.white)
-                                                .padding()
-                                        }
+                                Button {
+                                    
+                                } label: {
+                                    ZStack {
+                                        //then be easy to set w and h base on display size
+                                        RoundedRectangle(cornerRadius: op.buttonHeight)
+                                            .frame(
+                                                width: op.buttonWidth,
+                                                height: op.buttonHeight
+                                            )
+                                            .foregroundStyle(op.buttonColor)
+                                        Text(op.rawValue)
+                                            .font(.largeTitle)
+                                            .foregroundStyle(.white)
+                                            .padding()
                                     }
-                                }else {
-                                    Button {
-                                        
-                                    } label: {
-                                        ZStack {
-                                            /*:
-                                             - somehow can use button shape directly
-                                             - then be easy to set w and h base on display size 
-                                             
-                                             */
-                                            Circle().foregroundStyle(op.buttonColor)
-                                            Text(op.rawValue)
-                                                .font(.largeTitle)
-                                                .foregroundStyle(.white)
-                                                .padding()
-                                        }
-                                    }
-                                }
+                                }.padding(op.buttonPadding)
                             }
                         }
                     }
                 }
             }
-            .padding()
         }
     }
 }
--- Calculator/_Constant.swift
+++ Calculator/_Constant.swift
@@ -7,43 +7,60 @@
 
 import SwiftUI
 
-enum buttons: String {
-    case one = "1"
-    case two = "2"
-    case three = "3"
-    case four = "4"
-    case five = "5"
-    case six = "6"
-    case seven = "7"
-    case eigh = "8"
-    case nine = "9"
-    case zero = "0"
-    case dot = "."
-    case clear = "c"
-    case invert = "+/-"
-    case equal = "="
-    case percent = "%"
-    case divide = "÷"
-    case multiply = "x"
-    case add = "+"
-    case minus = "-"
-    
-    var buttonColor: Color {
-        switch self {
-        case .clear, .invert, .percent:
-            return .buttonGray
-        case .divide, .multiply, .minus, .add, .equal:
-            return .buttonOrange
-        default:
-            return .buttonDarkGray
-        }
-    }
-}
-
-let buttonLayout: [[buttons]] = [
-    [.clear, .invert, .percent, .divide],
-    [.seven, .eigh, .nine, .multiply],
-    [.four, .five, .six, .minus],
-    [.one, .two, .three, .add],
-    [.zero, .dot, .equal]
-]
+//enum buttons: String {
+//    case one = "1"
+//    case two = "2"
+//    case three = "3"
+//    case four = "4"
+//    case five = "5"
+//    case six = "6"
+//    case seven = "7"
+//    case eigh = "8"
+//    case nine = "9"
+//    case zero = "0"
+//    case dot = "."
+//    case clear = "c"
+//    case invert = "+/-"
+//    case equal = "="
+//    case percent = "%"
+//    case divide = "÷"
+//    case multiply = "x"
+//    case add = "+"
+//    case minus = "-"
+//    
+//    var buttonColor: Color {
+//        switch self {
+//        case .clear, .invert, .percent:
+//            return .buttonGray
+//        case .divide, .multiply, .minus, .add, .equal:
+//            return .buttonOrange
+//        default:
+//            return .buttonDarkGray
+//        }
+//    }
+//    
+//    var buttonWidth: CGFloat {
+//        switch self {
+//        case .zero:
+//            return buttonHeight * 2
+//        default:
+//            return buttonHeight
+//        }
+//    }
+//
+//    var buttonHeight: CGFloat {
+//        100
+//    }
+//    
+//    var buttonPadding: CGFloat {
+//        10
+//    }
+//}
+//
+//let buttonLayout: [[buttons]] = [
+//    [.clear, .invert, .percent, .divide],
+//    [.seven, .eigh, .nine, .multiply],
+//    [.four, .five, .six, .minus],
+//    [.one, .two, .three, .add],
+//    [.zero, .dot, .equal]
+//]
